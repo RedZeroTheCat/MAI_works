18fac3bea2abacf9af76f532b3ce7ce75c99710d
RedZeroTheCat <146474467+RedZeroTheCat@users.noreply.github.com>
2023-10-26 19:44:58 +0300

    Решение 9-ого задания 2-ой лабы

---
+++ semester 3/math_prac/lab 2/ex9/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#include "functions.c"
+
+int main() {
+    representation_check(16, 6, 0.5, 0.25, 0.125, 0.1, 0.2, 0.3);
+    printf("\n");
+    representation_check(10, 6, 0.5, 0.25, 0.125, 0.1, 0.2, 0.3);
+    printf("\n");
+    representation_check(8, 6, 0.5, 0.25, 0.125, 0.1, 0.2, 0.3);
+    printf("\n");
+    representation_check(5, 6, 0.5, 0.25, 0.125, 0.1, 0.2, 0.3);
+    printf("\n");
+    representation_check(3, 6, 0.5, 0.25, 0.125, 0.1, 0.2, 0.3);
+    printf("\n");
+    representation_check(2, 6, 0.5, 0.25, 0.125, 0.1, 0.2, 0.3);
+    return 0;
+}
